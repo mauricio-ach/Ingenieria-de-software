b291aac6b0f9e83dd6b2e28101b7375c0be2be88
Mauricio <mauricio.ach@ciencias.unam.mx>
2019-02-19 14:21:35 -0600

    Iniciando agregar usuario

--- Mapita/src/main/java/lab/mapita/Modelo/UsuarioDAO.java
+++ Mapita/src/main/java/lab/mapita/Modelo/UsuarioDAO.java
@@ -5,10 +5,63 @@
  */
 package lab.mapita.Modelo;
 
+import java.util.List;
+
 /**
  *
  * @author mauricio07
  */
 public class UsuarioDAO extends AbstractDAO<Usuario>{
     
+    /**
+     * 
+     */
+    public UsuarioDAO(){
+        super();
+    }
+    
+    /**
+     * 
+     * @param usuario 
+     */
+    @Override
+    public void save(Usuario usuario){
+        super.save(usuario);
+    }
+    
+    /**
+     * 
+     * @param usuario 
+     */
+    @Override
+    public  void update(Usuario usuario){
+        super.update(usuario);
+    }
+    
+    /**
+     * 
+     * @param usuario 
+     */
+    @Override
+    public void delete(Usuario usuario){
+        super.delete(usuario);
+    }
+    
+    /**
+     * 
+     * @param id
+     * @return 
+     */
+    public Usuario find(int id){
+        return super.find(Usuario.class, id);
+    }
+    
+    /**
+     * 
+     * @return 
+     */     
+    public List<Usuario> findAll(){
+        return super.findAll(Usuario.class);
+    
+    }
 }
